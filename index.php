486cdf59eea62c535ca731905dc3b22b1924d3ce
zakariae azhari <159003869+zakki-z@users.noreply.github.com>
2025-08-12 19:10:35 +0100

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ use Src\Router\Router;
 
 <html lang="en">
 <head>
-    <title>PHP Project</title>
+    <title>SkillConnect</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
